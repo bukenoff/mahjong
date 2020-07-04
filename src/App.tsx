f06fdbae3d06808c007f13fdc06d0617b6db63b9
serik <bukenoff.s@gmail.com>
2020-07-04 14:58:00 +0700

    added global styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { FC } from 'react';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import '~/styles/reset.scss';
+import '~/styles/global.scss';
 
 import GameView from './containers/GameView';
 
