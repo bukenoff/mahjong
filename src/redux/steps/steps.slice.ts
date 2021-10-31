a94b998481db1bdadd43f69a4cc083f7aa1f02f7
serik <bukenoff.s@gmail.com>
2021-10-31 16:02:12 +0700

    typed steps slice

--- src/redux/steps/steps.slice.ts
+++ src/redux/steps/steps.slice.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
-import { createSlice, PayloadAction, Action } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction, CaseReducer } from '@reduxjs/toolkit';
 import { Tile, StepsState } from '~/types';
 import { board_actions } from '../board/board.slice';
 
@@ -9,20 +9,33 @@ const initial_state: StepsState = {
   steps_made: 0,
 };
 
-const steps_slice = createSlice({
+type StepsReducer<T = undefined> = T extends undefined
+  ? (state: StepsState) => void
+  : CaseReducer<StepsState, PayloadAction<T>>;
+
+const steps_slice = createSlice<
+  StepsState,
+  {
+    stepIndexIncremented: StepsReducer;
+    stepIndexDecremented: StepsReducer;
+    stepAddedToStack: StepsReducer<{ tile_pair: [Tile, Tile] }>;
+    stepsMadeIncremented: StepsReducer;
+    takeStepBack: StepsReducer;
+    takeStepForward: StepsReducer;
+    stepBackTaken: StepsReducer;
+    stepForwardTaken: StepsReducer;
+  }
+>({
   name: 'steps',
   initialState: initial_state,
   reducers: {
-    stepIndexIncremented: (state, action: Action) => {
+    stepIndexIncremented: (state) => {
       state.step_index = state.step_index + 1;
     },
-    stepIndexDecremented: (state, action: Action) => {
+    stepIndexDecremented: (state) => {
       state.step_index = state.step_index - 1;
     },
-    stepAddedToStack: (
-      state,
-      action: PayloadAction<{ tile_pair: [Tile, Tile] }>,
-    ) => {
+    stepAddedToStack: (state, action) => {
       const { tile_pair } = action.payload;
       const { step_index } = state;
       let { steps_stack } = state;
@@ -33,13 +46,13 @@ const steps_slice = createSlice({
         steps_stack = steps_stack.slice(0, step_index + 1);
       }
     },
-    stepsMadeIncremented: (state, action: Action) => {
+    stepsMadeIncremented: (state) => {
       state.steps_made = state.steps_made + 1;
     },
-    takeStepBack: (state, action: Action) => {},
-    takeStepForward: (state, action: Action) => {},
-    stepBackTaken: (state, action: Action) => {},
-    stepForwardTaken: (state, action: Action) => {},
+    takeStepBack: () => {},
+    takeStepForward: () => {},
+    stepBackTaken: () => {},
+    stepForwardTaken: () => {},
   },
   extraReducers: (builder) =>
     builder.addCase(board_actions.newBoardGenerated, (state) => {
