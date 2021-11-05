4a316286b085d84532f5553368c057acf11e6d2e
serik <bukenoff.s@gmail.com>
2021-11-05 22:32:14 +0700

    Moved start game to start view

--- src/containers/GameView/GameView.tsx
+++ src/containers/GameView/GameView.tsx
@@ -1,19 +1,12 @@
-import React, { FC, useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import React, { FC } from 'react';
+import { useSelector } from 'react-redux';
 import Board from '~/components/Board';
 import GameControlPanel from '~/containers/GameControlPanel';
 import * as styles from './styles.scss';
 import { selectIsStopped } from '~/redux/timer/timer.selectors';
-import { actions } from '~/redux';
 
 export const GameView: FC = () => {
   const is_stopped = useSelector(selectIsStopped);
-
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(actions.generateNewBoard());
-  }, []);
 
   return (
     <div className={styles.container}>
--- src/containers/StartView/StartView.tsx
+++ src/containers/StartView/StartView.tsx
@@ -1,17 +1,45 @@
-import React, { FC } from 'react';
+import React, { ChangeEventHandler, FC, useCallback, useState } from 'react';
+import { useHistory } from 'react-router';
 import * as styles from './styles.scss';
 import { Link } from 'react-router-dom';
 import { PATHS } from '~/constants/paths';
+import { useDispatch } from 'react-redux';
+import { actions } from '~/redux';
 
 export const StartView: FC = () => {
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const [name, setName] = useState('');
+
+  const onNameChange: ChangeEventHandler<HTMLInputElement> = useCallback(
+    (e) => {
+      setName(e.currentTarget.value);
+    },
+    [],
+  );
+
+  const onStartGameClick = () => {
+    dispatch(actions.generateNewBoard());
+    history.push(PATHS.GAME);
+  };
+
   return (
     <div className={styles.container}>
-      <button type="button" className={styles.start_button}>
-        <Link to={PATHS.GAME}>Start game</Link>
-      </button>
-      <button type="button" className={styles.start_button}>
-        <Link to={PATHS.SCORES}>Scores</Link>
-      </button>
+      <label htmlFor="name">Enter your name sir</label>
+      <input type="text" id="name" value={name} onChange={onNameChange} />
+      <>
+        <button
+          type="button"
+          className={styles.start_button}
+          onClick={onStartGameClick}
+          disabled={!name.length}
+        >
+          Start game
+        </button>
+        <button type="button" className={styles.start_button}>
+          <Link to={PATHS.SCORES}>Scores</Link>
+        </button>
+      </>
     </div>
   );
 };
